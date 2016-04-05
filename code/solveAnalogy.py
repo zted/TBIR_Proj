ec6c10feece1b4007f3416cef9532b9b61267926
zted <zhangteddy22@gmail.com>
2016-04-05 23:53:05 +0200

    first cosine similarity implemented

--- code/solveAnalogy.py
+++ code/solveAnalogy.py
@@ -1,44 +1,64 @@
-filename = '../questions-words.txt'
+import linecache as lc
+import numpy as np
+import scipy.spatial as spat
 
-count = 0
-with open(filename, 'r') as f:
-    lines = f.read().splitlines()
-    for line in lines:
-        words = line.split(' ')
-        print(words)
-        if words[0] == ':':
-            continue
-        a = words[0]
-        b = words[1]
-        c = words[2]
-        answer = words[3]
-        count += 1
-        if count > 5:
-            break
+embeddingsFile = '../data/glove.6B/glove.6B.50d.txt'
 
-filename = '../glove.6B/glove.6B.50d.txt'
 
-def createIndices(someFile):
-    someDict = {}
+def create_indices(file):
+    d = {}
     count = 0
-    with open(filename, mode='r', encoding="ISO-8859-1") as f:
+    with open(file, mode='r', encoding="ISO-8859-1") as f:
         lines = f.read().splitlines()
         for line in lines:
             count += 1
             word = line.split(' ')[0]
-            someDict[word] = count
-    return someDict
+            d[word] = count
+    return d
 
 
+def get_vector(file, line):
+    v = lc.getline(file, line)
+    v = v.strip('\n').split(' ')[1:]
+    return np.array(list(map(float, v)))
 
 
-num_lines = sum(1 for line in open(filename, encoding="ISO-8859-1"))
-print(num_lines)
-a = [1] * (3000000)
-# count = 0
-# with open(filename,'r') as f:
-#     for i,l in enumerate(f):
-#             pass
-#     print(i+1)
+def fetch_most_similar(vect, file, ignore_word):
+    with open(file, mode='r', encoding="ISO-8859-1") as f:
+        best_word = 'yo_mama'
+        highest = -1000
+        for line in f:
+            v = line.strip('\n').split(' ')
+            word = v[0]
+            if word == ignore_word:
+                continue
+            v = np.array(list(map(float, v[1:])))
+            sim_measure = 1 - spat.distance.cosine(vect, v)
+            # cosine similarity
+            (best_word, highest) = (word, sim_measure) \
+                if sim_measure > highest else (best_word, highest)
+    return best_word
 
-# def fetchVectors(a,b,c)
+wordIndices = create_indices(embeddingsFile)
+
+filename = '../data/questions-words.txt'
+
+count = 0
+with open(filename, 'r') as f:
+    lines = f.read().splitlines()
+    for l in lines:
+        words = l.split(' ')
+        if words[0] == ':':
+            continue
+        a = words[0].lower()
+        b = words[1].lower()
+        c = words[2].lower()
+        answer = words[3].lower()
+        count += 1
+        vecA = get_vector(embeddingsFile, wordIndices[a])
+        vecB = get_vector(embeddingsFile, wordIndices[b])
+        vecC = get_vector(embeddingsFile, wordIndices[c])
+        resultant = vecC + vecB - vecA
+        print(fetch_most_similar(resultant, embeddingsFile, c))
+        if count > 5:
+            break
