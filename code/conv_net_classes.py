e5abaf152e7e31093ccc69a892afceced8814dc3
zted <zhangteddy22@gmail.com>
2016-04-15 23:29:55 +0200

    Creating own version of logistic regression and last layer

--- code/conv_net_classes.py
+++ code/conv_net_classes.py
@@ -234,7 +234,7 @@ class MLP(object):
         # the parameters of the model are the parameters of the two layer it is
         # made out of
         self.params = self.hiddenLayer.params + self.logRegressionLayer.params
-        
+
 class LogisticRegression(object):
     """Multi-class Logistic Regression Class
 
@@ -250,15 +250,15 @@ class LogisticRegression(object):
     :type input: theano.tensor.TensorType
     :param input: symbolic variable that describes the input of the
     architecture (one minibatch)
-    
+
     :type n_in: int
     :param n_in: number of input units, the dimension of the space in
     which the datapoints lie
-    
+
     :type n_out: int
     :param n_out: number of output units, the dimension of the space in
     which the labels lie
-    
+
     """
 
         # initialize with 0 the weights W as a matrix of shape (n_in, n_out)
@@ -292,15 +292,15 @@ class LogisticRegression(object):
         of this model under a given target distribution.
 
     .. math::
-    
+
     \frac{1}{|\mathcal{D}|} \mathcal{L} (\theta=\{W,b\}, \mathcal{D}) =
     \frac{1}{|\mathcal{D}|} \sum_{i=0}^{|\mathcal{D}|} \log(P(Y=y^{(i)}|x^{(i)}, W,b)) \\
     \ell (\theta=\{W,b\}, \mathcal{D})
-    
+
     :type y: theano.tensor.TensorType
     :param y: corresponds to a vector that gives for each example the
     correct label
-    
+
     Note: we use the mean instead of the sum so that
     the learning rate is less dependent on the batch size
     """
@@ -319,7 +319,7 @@ class LogisticRegression(object):
     def errors(self, y):
         """Return a float representing the number of errors in the minibatch ;
     zero one loss over the size of the minibatch
-    
+
     :type y: theano.tensor.TensorType
     :param y: corresponds to a vector that gives for each example the
     correct label
@@ -328,7 +328,7 @@ class LogisticRegression(object):
         # check if y has same dimension of y_pred
         if y.ndim != self.y_pred.ndim:
             raise TypeError('y should have the same shape as self.y_pred',
-                ('y', target.type, 'y_pred', self.y_pred.type))
+                ('y', y.type, 'y_pred', self.y_pred.type))
         # check if y is of the correct datatype
         if y.dtype.startswith('int'):
             # the T.neq operator returns a vector of 0s and 1s, where 1
@@ -336,7 +336,113 @@ class LogisticRegression(object):
             return T.mean(T.neq(self.y_pred, y))
         else:
             raise NotImplementedError()
-        
+
+
+class LogisticRegression2(object):
+    """Multi-class Logistic Regression Class
+
+    The logistic regression is fully described by a weight matrix :math:`W`
+    and bias vector :math:`b`. Classification is done by projecting data
+    points onto a set of hyperplanes, the distance to which is used to
+    determine a class membership probability.
+    """
+
+    def __init__(self, input, n_in, n_out, W=None, b=None):
+        """ Initialize the parameters of the logistic regression
+
+    :type input: theano.tensor.TensorType
+    :param input: symbolic variable that describes the input of the
+    architecture (one minibatch)
+
+    :type n_in: int
+    :param n_in: number of input units, the dimension of the space in
+    which the datapoints lie
+
+    :type n_out: int
+    :param n_out: number of output units, the dimension of the space in
+    which the labels lie
+
+    """
+
+        # initialize with 0 the weights W as a matrix of shape (n_in, n_out)
+        if W is None:
+            self.W = theano.shared(
+                    value=numpy.zeros((n_in, n_out), dtype=theano.config.floatX),
+                    name='W')
+        else:
+            self.W = W
+
+        # initialize the baises b as a vector of n_out 0s
+        if b is None:
+            self.b = theano.shared(
+                    value=numpy.zeros((n_out,), dtype=theano.config.floatX),
+                    name='b')
+        else:
+            self.b = b
+
+        # compute vector of class-membership probabilities in symbolic form
+        self.p_y_given_x = T.dot(input, self.W) + self.b
+
+        # compute prediction as class whose probability is maximal in
+        # symbolic form
+        self.y_pred = self.p_y_given_x
+
+        # parameters of the model
+        self.params = [self.W, self.b]
+
+    def negative_log_likelihood(self, y):
+        """Return the mean of the negative log-likelihood of the prediction
+        of this model under a given target distribution.
+
+    .. math::
+
+    \frac{1}{|\mathcal{D}|} \mathcal{L} (\theta=\{W,b\}, \mathcal{D}) =
+    \frac{1}{|\mathcal{D}|} \sum_{i=0}^{|\mathcal{D}|} \log(P(Y=y^{(i)}|x^{(i)}, W,b)) \\
+    \ell (\theta=\{W,b\}, \mathcal{D})
+
+    :type y: theano.tensor.TensorType
+    :param y: corresponds to a vector that gives for each example the
+    correct label
+
+    Note: we use the mean instead of the sum so that
+    the learning rate is less dependent on the batch size
+    """
+        # y.shape[0] is (symbolically) the number of rows in y, i.e.,
+        # number of examples (call it n) in the minibatch
+        # T.arange(y.shape[0]) is a symbolic vector which will contain
+        # [0,1,2,... n-1] T.log(self.p_y_given_x) is a matrix of
+        # Log-Probabilities (call it LP) with one row per example and
+        # one column per class LP[T.arange(y.shape[0]),y] is a vector
+        # v containing [LP[0,y[0]], LP[1,y[1]], LP[2,y[2]], ...,
+        # LP[n-1,y[n-1]]] and T.mean(LP[T.arange(y.shape[0]),y]) is
+        # the mean (across minibatch examples) of the elements in v,
+        # i.e., the mean log-likelihood across the minibatch.
+        return T.sum(abs(self.y_pred - y))
+
+    def errors(self, y):
+        """Return a float representing the number of errors in the minibatch ;
+    zero one loss over the size of the minibatch
+
+    :type y: theano.tensor.TensorType
+    :param y: corresponds to a vector that gives for each example the
+    correct label
+    """
+        # check if y has same dimension of y_pred
+        if y.ndim != self.y_pred.ndim:
+            print(self.y_pred.ndim)
+            T.cast
+            print(y.ndim)
+            raise TypeError('y should have the same shape as self.y_pred',
+                ('y', y.type, 'y_pred', self.y_pred.type))
+        # check if y is of the correct datatype
+        if y.dtype.startswith('float'):
+            # the T.neq operator returns a vector of 0s and 1s, where 1
+            # represents a mistake in prediction
+            return T.sum(abs(self.y_pred - y))
+        else:
+            raise NotImplementedError()
+
+
 class LeNetConvPoolLayer(object):
     """Pool Layer of a convolutional network """
 
@@ -416,3 +522,88 @@ class LeNetConvPoolLayer(object):
             output = pooled_out + self.b.dimshuffle('x', 0, 'x', 'x')
         return output
         
+class Dropout(object):
+    """A multilayer perceptron with dropout"""
+    def __init__(self,rng,input,layer_sizes,dropout_rates,activations,use_bias=True):
+
+        #rectified_linear_activation = lambda x: T.maximum(0.0, x)
+
+        # Set up all the hidden layers
+        self.weight_matrix_sizes = zip(layer_sizes, layer_sizes[1:])
+        self.layers = []
+        self.dropout_layers = []
+        self.activations = activations
+        next_layer_input = input
+        #first_layer = True
+        # dropout the input
+        next_dropout_layer_input = _dropout_from_layer(rng, input, p=dropout_rates[0])
+        layer_counter = 0
+        for n_in, n_out in self.weight_matrix_sizes[:-1]:
+            next_dropout_layer = DropoutHiddenLayer(rng=rng,
+                    input=next_dropout_layer_input,
+                    activation=activations[layer_counter],
+                    n_in=n_in, n_out=n_out, use_bias=use_bias,
+                    dropout_rate=dropout_rates[layer_counter])
+            self.dropout_layers.append(next_dropout_layer)
+            next_dropout_layer_input = next_dropout_layer.output
+
+            # Reuse the parameters from the dropout layer here, in a different
+            # path through the graph.
+            next_layer = HiddenLayer(rng=rng,
+                    input=next_layer_input,
+                    activation=activations[layer_counter],
+                    # scale the weight matrix W with (1-p)
+                    W=next_dropout_layer.W * (1 - dropout_rates[layer_counter]),
+                    b=next_dropout_layer.b,
+                    n_in=n_in, n_out=n_out,
+                    use_bias=use_bias)
+            self.layers.append(next_layer)
+            next_layer_input = next_layer.output
+            #first_layer = False
+            layer_counter += 1
+
+        # Set up the output layer
+        n_in, n_out = self.weight_matrix_sizes[-1]
+        dropout_output_layer = LogisticRegression2(
+                input=next_dropout_layer_input,
+                n_in=n_in, n_out=n_out)
+        self.dropout_layers.append(dropout_output_layer)
+
+        # Again, reuse paramters in the dropout output.
+        output_layer = LogisticRegression2(
+            input=next_layer_input,
+            # scale the weight matrix W with (1-p)
+            W=dropout_output_layer.W * (1 - dropout_rates[-1]),
+            b=dropout_output_layer.b,
+            n_in=n_in, n_out=n_out)
+        self.layers.append(output_layer)
+
+        # Use the negative log likelihood of the logistic regression layer as
+        # the objective.
+        self.dropout_negative_log_likelihood = self.dropout_layers[-1].negative_log_likelihood
+        self.dropout_errors = self.dropout_layers[-1].errors
+
+        self.negative_log_likelihood = self.layers[-1].negative_log_likelihood
+        self.errors = self.layers[-1].errors
+
+        # Grab all the parameters together.
+        self.params = [ param for layer in self.dropout_layers for param in layer.params ]
+
+    def predict(self, new_data):
+        next_layer_input = new_data
+        for i,layer in enumerate(self.layers):
+            if i<len(self.layers)-1:
+                next_layer_input = self.activations[i](T.dot(next_layer_input,layer.W) + layer.b)
+            else:
+                p_y_given_x = T.dot(next_layer_input, layer.W) + layer.b
+        y_pred = p_y_given_x
+        return y_pred
+
+    def predict_p(self, new_data):
+        next_layer_input = new_data
+        for i,layer in enumerate(self.layers):
+            if i<len(self.layers)-1:
+                next_layer_input = self.activations[i](T.dot(next_layer_input,layer.W) + layer.b)
+            else:
+                p_y_given_x = T.dot(next_layer_input, layer.W) + layer.b
+        return p_y_given_x
