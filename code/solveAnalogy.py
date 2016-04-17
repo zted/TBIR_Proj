21f474f0244d6f3b10ed1473331333f191ce4479
zted <zhangteddy22@gmail.com>
2016-04-17 22:50:48 +0200

    Aesthetic fixes

--- code/solveAnalogy.py
+++ code/solveAnalogy.py
@@ -1,12 +1,13 @@
 import numpy as np
 import time
 import gensim
+
 t0 = time.time()
 
 
 def unit_vec(v):
     mag = np.linalg.norm(v)
-    tempArray = v * 0 if mag == 0 else v/mag
+    tempArray = v * 0 if mag == 0 else v / mag
     return tempArray
 
 
@@ -15,7 +16,7 @@ def fetch_most_similar(vect, mat, model, ignore_word):
     index = np.argmax(resultant)
     word = model.index2word[index]
     if word == ignore_word:
-        resultant[0,index] = -1000
+        resultant[0, index] = -1000
         # ^we do argmax again to get second best word, since first best is itself
         index = np.argmax(resultant)
         word = model.index2word[index]
@@ -23,21 +24,22 @@ def fetch_most_similar(vect, mat, model, ignore_word):
 
 
 def cos_sim_addition(a, b, c, matrix, model, ignore_word):
-    resultant = c+b-a
+    resultant = c + b - a
     return fetch_most_similar(resultant, matrix, model, ignore_word)
 
 
 def cos_sim_direction(a, b, c, matrix, file, ignore_word):
-    resultant = a-b
+    resultant = a - b
     newMatrix = []
     for r in matrix:
-        tempArray = c-np.squeeze(np.asarray(r))
+        tempArray = c - np.squeeze(np.asarray(r))
         newMatrix.append(unit_vec(tempArray))
     return fetch_most_similar(resultant, np.asmatrix(newMatrix), file, ignore_word)
 
 
 def cos_sim_multiplication(a, b, c, matrix, file, ignore_word):
     return
+
 
 questionsFile = '../data/questions-words.txt'
 count = 0
@@ -51,7 +53,7 @@ embFileName = 'glove.6B.{0}d.txt'.format(50)
 embeddingsFile = '../data/glove.6B/' + embFileName
 outFile = '../results/accuracy_' + embFileName
 model = gensim.models.Word2Vec.load_word2vec_format(embeddingsFile, binary=False)
-model.init_sims(replace=True) # indicates we're finished training to save ram
+model.init_sims(replace=True)  # indicates we're finished training to save ram
 makeLowerCase = True
 
 # embeddingsFile = '../data/GoogleNews-vectors-negative300.bin'
@@ -77,10 +79,10 @@ with open(questionsFile, 'r') as f:
                 of.write(' '.join(words[1:]) + ':\n')
                 skipFlag = False
                 continue
-            recall = n_corr*100/(n_corr+n_incorr)
-            total_recall = total_corr*100/(total_corr+total_incorr)
+            recall = n_corr * 100 / (n_corr + n_incorr)
+            total_recall = total_corr * 100 / (total_corr + total_incorr)
             of.write('RECALL: {0:.2f} %  ({1:d} / {2:d})\n'
-                     .format(recall, n_corr, n_corr+n_incorr))
+                     .format(recall, n_corr, n_corr + n_incorr))
             of.write('Total recall: {0:.2f} %\n'.format(total_recall))
             n_corr = 0
             n_incorr = 0
@@ -107,12 +109,12 @@ with open(questionsFile, 'r') as f:
         else:
             n_incorr += 1
             total_incorr += 1
-        # if count > 5:
-        #     break
-    recall = n_corr*100/(n_corr+n_incorr)
-    total_recall = total_corr*100/(total_corr+total_incorr)
+            # if count > 5:
+            #     break
+    recall = n_corr * 100 / (n_corr + n_incorr)
+    total_recall = total_corr * 100 / (total_corr + total_incorr)
     of.write('RECALL: {0:.2f} %  ({1:d} / {2:d})\n'
-                .format(recall, n_corr, n_corr+n_incorr))
+             .format(recall, n_corr, n_corr + n_incorr))
     of.write('Total recall: {0:.2f} %\n'.format(total_recall))
     t_elapsed = time.time() - t0
     print("Unfound words: ", unfound_words)
