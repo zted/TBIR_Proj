fdf86166de075594d3ab4f8ecb86524433ee9c5b
zted <zhangteddy22@gmail.com>
2016-04-27 10:37:14 +0200

    Corrected counting

--- code/createTestingData.py
+++ code/createTestingData.py
@@ -14,20 +14,16 @@ def createTestingExamples(num_examples, dim):
     testing_file_y = '../data/test_y_{}d_{}.txt'.format(dim, num_examples)
     testing_file_x = '../data/test_x_{}.txt'.format(num_examples)
     indices.sort()
-    count = 1
-    first = True
+    count = 0
     fo_x = open(testing_file_x, 'w')
     fo_y = open(testing_file_y, 'w')
     fo_y.write('{} {}\n'.format(num_examples, dim))
 
     with open(training_file, 'r') as f:
         for n, line in enumerate(f):
-            if count > num_examples:
+            if count >= num_examples:
                 break
-            if first:
-                first = False
-                continue
-            if n - 1 == indices[count]:
+            if n == indices[count]:
                 # we get the lines at the random indices and get the image ID for that line
                 image_id = line.split(' ')[0]
                 idx = image_dict[image_id]
@@ -52,7 +48,7 @@ if __name__ == "__main__":
     try:
         output_dim = int(sys.argv[2])
     except IndexError:
-        output_dim = 400
+        output_dim = 200
 
     assert (num_training in num_train_opt and
             output_dim in output_opt)
