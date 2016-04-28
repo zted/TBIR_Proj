d004f536ea6be450f27bf55aee41ebfffd9e93e7
zted <zhangteddy22@gmail.com>
2016-04-28 22:47:56 +0200

    Adding comments

--- code/createTestingData.py
+++ code/createTestingData.py
@@ -4,15 +4,22 @@ import sys
 
 
 def createTestingExamples(num_examples, dim):
-    image_embeddings = '../data/visfeat_train_reduced_tr{}.txt'.format(dim)
+    """
+    Creates practice examples for us to evaluate how many relevant images
+    we can retrieve with our models
+    :param num_examples: number of examples to create
+    :param dim: visual vector dimensions
+    :return:
+    """
+    image_embeddings = '../data/visfeat_train_reduced_{}.txt'.format(dim)
     image_dict, _ = hf.create_indices_for_vectors(image_embeddings)
 
     indices = np.random.choice(310111, num_examples, False)
     # randomly polls from the training data we have to make a test set
     # first we create random indices
     training_file = '../data/train_data.txt'
-    testing_file_y = '../data/test_y_{}d_{}.txt'.format(dim, num_examples)
-    testing_file_x = '../data/test_x_{}d_{}.txt'.format(dim, num_examples)
+    testing_file_y = '../results/test_y_{}d_{}.txt'.format(dim, num_examples)
+    testing_file_x = '../results/test_x_{}d_{}.txt'.format(dim, num_examples)
     indices.sort()
     count = 0
     fo_x = open(testing_file_x, 'w')
@@ -37,13 +44,13 @@ def createTestingExamples(num_examples, dim):
 
 if __name__ == "__main__":
 
-    num_train_opt = [5, 50, 500, 5000, 10000]
+    num_train_opt = [5, 50, 500, 5000, 10000, 50000]
     output_opt = [200, 400]
 
     try:
         num_training = int(sys.argv[1])
     except IndexError:
-        num_training = 2000
+        num_training = 5000
 
     try:
         output_dim = int(sys.argv[2])
